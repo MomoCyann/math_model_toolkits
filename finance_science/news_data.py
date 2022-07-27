489f2420ed49ba24ece2cb42bcf968b71c1b50ab
MomoCyann <541525127@qq.com>
2022-07-27 10:50:41 +0800

    7/27 新闻去重

--- finance_science/news_data.py
+++ finance_science/news_data.py
@@ -8,39 +8,42 @@ import glob
 
 def del_same_news(data):
     '''
-    新闻数据去重。
+    新闻数据去重。方法：把需要保存下来的新闻，包括独一无二的、和重复里面决定留下来的新闻的ID保存下来，在原始数据中保留这些ID的行。
     :param data:
     :return:
     '''
+    # 复制一份数据
+    data_copy = data.copy()
+    # 去掉标点符号 方便比较
+    data_copy['newsTitle'] = data_copy['newsTitle'].apply(lambda x: re.sub(r'[\W]', "", str(x)))
+    data_copy['newsSummary'] = data_copy['newsSummary'].apply(lambda x: re.sub(r'[\W]', "", str(x)))
     # 重复数据全部去除→clean
-    clean = data.drop_duplicates(subset=['newsTitle'], keep=False)
+    clean = data_copy.drop_duplicates(subset=['newsTitle'], keep=False)
     # 这个操作可以单独取出重复新闻
-    duplic = data.append(clean).drop_duplicates(keep=False)
+    duplic = data_copy.append(clean).drop_duplicates(keep=False)
     # 按时间与标题排序
     duplic = duplic.sort_values(by=['newsPublishTime','newsTitle'])
-    # 去掉标点符号
-    duplic['newsTitle'] = duplic['newsTitle'].apply(lambda x: re.sub(r'[\W]', "", str(x)))
-    duplic['newsSummary'] = duplic['newsSummary'].apply(lambda x: re.sub(r'[\W]', "", str(x)))
+
 
     # 优先保留来源者等于发布者
     # 先根据标题分组
-    news_group = list(duplic.groupby(by='newsSummary'))
+    news_group = list(duplic.groupby(by='newsTitle'))
     # 需留存的的新闻ID
-    news_ID = []
+    # clean新闻的ID
+    news_ID = list(clean.iloc[:, 0])
     for i in news_group:
         # 对于每一组，先检验有没有来源者等于发布者的新闻
         if (i[1]['newsOriginSource']==i[1]['newsPublishSite']).any():
-            news_ID.append(i[1].iloc[:,0].loc[i[1]['newsOriginSource']==i[1]['newsPublishSite']].iloc[0])
+            news_ID.append(i[1].iloc[:, 0].loc[i[1]['newsOriginSource']==i[1]['newsPublishSite']].iloc[0])
         # 否则选最早的
         else:
             news_ID.append(i[1].iloc[0, 0])
     # 根据ID保留新闻
-    duplic_source = duplic.loc[news_ID, :]
-    # 与无重复的新闻合并
-    data = pd.concat([clean, duplic_source], axis=0)
+    data = data.loc[news_ID, :]
     # 排序
     data.sort_index(inplace=True)
     data.reset_index(inplace=True, drop=True)
+    data.index = data.index + 1
     data = data.iloc[:,1:]
     return data
 
@@ -62,9 +65,5 @@ def clear_data():
 
 if __name__ == "__main__":
 
-    #clear_data()
-    data = pd.read_csv("./data/海康威视1-12.csv")
-    print(data.info)
-    data = del_same_news(data)
-    print('data cleared')
-    print(data.info)
+    clear_data()
+
