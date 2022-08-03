208ce2877045245f694b7fde419ce24204cb8649
MomoCyann <541525127@qq.com>
2022-08-03 20:09:46 +0800

    8/3 NN/LSTM/BiLSTM

--- finance_science/NN_classifier.py
+++ finance_science/NN_classifier.py
@@ -8,11 +8,10 @@ from keras.utils.np_utils import *
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import recall_score, precision_score, f1_score
 from sklearn.metrics import classification_report
-import keras_metrics
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
+from keras.layers import Dense, Dropout, LSTM, Bidirectional
+
 
 '''
 0 - 中性
@@ -72,37 +71,98 @@ def load_data(path):
     print(len(val), 'val examples')
     return train, test, val
 
+def reshape_to_3d(array):
+    rows = array.shape[0]
+    cols = array.shape[1]
+    array = array.reshape((rows,1,cols))
+    return array
+
+def net_NN():
+    # 全连接网络 76%
+    model = Sequential()
+    model.add(Dense(256, activation='relu',input_shape=(768,),name='layer1'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=30, batch_size=64,
+                        validation_data=(x_val, y_val), verbose=1, callbacks=Metrics(valid_data=(x_val, y_val)))
+    loss, accuracy = model.evaluate(x_test, y_test)
+    print("Accuracy", accuracy)
+    # 保存模型
+    model.save('./BERT/model/NN')
+
+def net_LSTM():
+    # LSTM
+    model = Sequential()
+    model.add(LSTM(units=256, return_sequences=True, input_shape=(1, 768)))
+    model.add(LSTM(units=128))
+    model.add(Dropout(0.2))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=800, batch_size=64,
+                        validation_data=(x_val, y_val), verbose=1, callbacks=Metrics(valid_data=(x_val, y_val)))
+    loss, accuracy = model.evaluate(x_test, y_test)
+    print("Accuracy", accuracy)
+    #保存模型
+    model.save('./BERT/model/LSTM')
+    # #加载模型
+    # model = tf.keras.models.load_model('./model/text_classifier')
+
+def net_BiLSTM():
+    # BiLSTM
+    model = Sequential()
+    model.add(Bidirectional(LSTM(units=256,return_sequences=True),input_shape=(1, 768)))
+    model.add(Bidirectional(LSTM(units=128)))
+    model.add(Dropout(0.5))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=30, batch_size=64,
+                        validation_data=(x_val, y_val), verbose=1, callbacks=Metrics(valid_data=(x_val, y_val)))
+    loss, accuracy = model.evaluate(x_test, y_test)
+    print("Accuracy", accuracy)
+    #保存模型
+    model.save('./BERT/model/BiLSTM')
+    # #加载模型
+    # model = tf.keras.models.load_model('./model/text_classifier')
 
 if __name__ == '__main__':
     # 读取数据
     path = 'BERT/embedding_title_label_v2.csv'
     train, test, val = load_data(path)
 
-    batch_size = 64
     x_train, y_train = df_to_tensor(train)
     x_test, y_test = df_to_tensor(test)
     x_val, y_val = df_to_tensor(val)
 
-    # 全连接网络
-    model = Sequential()
-    model.add(Dense(256, activation='relu',input_shape=(768,),name='layer1'))
-    model.add(Dense(128, activation='relu',name='layer2'))
-    model.add(Dropout(0.5,name='dropout'))
-    model.add(Dense(3, activation='softmax',name='output'))
-
+    # 直接分类 72%
     # model = Sequential()
-    # model.add(Dense(3, activation='softmax',input_shape=(768,),name='output'))
+    # model.add(Dense(3, activation='softmax',input_shape=(768,)))
 
+    # 若选用LSTM
+    # 先把X的2维数组变成3维数组, 不用重塑Y
+    x_train, x_test, x_val = reshape_to_3d(x_train), reshape_to_3d(x_test), reshape_to_3d(x_val)
 
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    history = model.fit(x_train, y_train ,epochs = 30, batch_size=64,
-                        validation_data=(x_val,y_val), verbose = 1, callbacks=Metrics(valid_data=(x_val,y_val)))
-    loss, accuracy = model.evaluate(x_test, y_test)
-    print("Accuracy", accuracy)
-
-    # #保存模型
-    # model.save('./model/text_classifier')
-    # #加载模型
-    # model = tf.keras.models.load_model('./model/text_classifier')
+    '''
+    只需在下方调整方法名，即可选用不同网络训练
+    注意：注释上方的重塑数组shape，在LSTM取消注释
+    结果：
+    # 全连接 epoch-30
+    loss: 0.5125 - accuracy: 0.7673 - val_loss: 0.5351 - val_accuracy: 0.7592 - val_f1: 0.7592 - val_recall: 0.7592 - val_precision: 0.7592
+    测试集 Accuracy 0.758499026298523
+    
+    # LSTM epoch-30
+    loss: 0.4807 - accuracy: 0.7843 - val_loss: 0.5275 - val_accuracy: 0.7616 - val_f1: 0.7616 - val_recall: 0.7616 - val_precision: 0.7616
+    测试集 Accuracy 0.7653766870498657
+    
+    # BiLSTM epoch-30
+    loss: 0.4779 - accuracy: 0.7882 - val_loss: 0.5171 - val_accuracy: 0.7643 - val_f1: 0.7643 - val_recall: 0.7643 - val_precision: 0.7643
+    测试集 Accuracy 0.76434326171875
+    '''
+    net_LSTM()
